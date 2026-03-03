924b8b2ad32d8b503b5eaa96c633500d3b4f82d6
Jouri-IT <jouri.f.alsulaiman@gmail.com>
2026-03-03 20:23:15 +0000

    task 4

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_most_numbers - prints the numbers from 0 to 9, except 2 and 4
  *
- * Return: Always 0.
- */#include "main.h"
-
+ * Return: void
+ */
 void print_most_numbers(void)
 {
 	int i;
