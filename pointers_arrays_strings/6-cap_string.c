c771dad39c4bb711e99f2b6635f0fab97e3e7ac2
root <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-03-05 21:09:36 +0000

    Add task 6: cap_string

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @s: The string to be modified.
+ *
+ * Return: A pointer to the modified string.
+ */
+char *cap_string(char *s)
+{
+	int i = 0, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	while (s[i] != '\0')
+	{
+		/* Check if first character is lowercase */
+		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
+			s[i] -= 32;
+
+		/* Check if current char is lowercase and preceded by a separator */
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
