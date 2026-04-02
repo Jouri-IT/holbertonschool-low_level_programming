1898d26e735308bcf50188968fd7c3a118914514
root <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-04-02 13:44:42 +0000

    Add task 0: create_array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+    char *array;
+    unsigned int i;
+
+    if (size == 0)
+        return (NULL);
+
+    array = malloc(sizeof(char) * size);
+    if (array == NULL)
+        return (NULL);
+
+    i = 0;
+    while (i < size)
+    {
+        array[i] = c;
+        i++;
+    }
+    return (array);
+}
