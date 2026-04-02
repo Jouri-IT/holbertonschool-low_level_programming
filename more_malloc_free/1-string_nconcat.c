82d64adc9e988b2307126a1fb0703a6c54727cb3
root <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-04-02 15:01:56 +0000

    Fix Betty: shorten string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -5,51 +5,40 @@
  * string_nconcat - concatenates two strings
  * @s1: first string
  * @s2: second string
- * @n: number of bytes of s2 to concatenate
+ * @n: number of bytes of s2 to use
  *
- * Return: pointer to the concatenated string, or NULL if it fails
+ * Return: pointer to new string, or NULL if fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *concat;
-	unsigned int i;
-	unsigned int j;
-	unsigned int len1;
-	unsigned int len2;
+	unsigned int i, j, len1, len2;
 
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
 	len1 = 0;
-	while (s1[len1] != '\0')
+	while (s1[len1])
 		len1++;
-
 	len2 = 0;
-	while (s2[len2] != '\0')
+	while (s2[len2])
 		len2++;
-
 	if (n >= len2)
 		n = len2;
-
-	concat = malloc(sizeof(char) * (len1 + n + 1));
+	concat = malloc(len1 + n + 1);
 	if (concat == NULL)
 		return (NULL);
-
 	i = 0;
 	while (i < len1)
 	{
 		concat[i] = s1[i];
 		i++;
 	}
-
 	j = 0;
 	while (j < n)
 	{
-		concat[i] = s2[j];
-		i++;
-		j++;
+		concat[i++] = s2[j++];
 	}
 	concat[i] = '\0';
 	return (concat);
