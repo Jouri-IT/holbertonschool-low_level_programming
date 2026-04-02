307c4f60fcdc18d1eba2e31636825e5e65ce209c
root <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-04-02 13:51:39 +0000

    Fix Betty style in create_array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,23 +1,30 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * create_array - creates an array of chars
+ * @size: size of the array
+ * @c: char to initialize the array with
+ *
+ * Return: pointer to the array, or NULL if it fails
+ */
 char *create_array(unsigned int size, char c)
 {
-    char *array;
-    unsigned int i;
+	char *array;
+	unsigned int i;
 
-    if (size == 0)
-        return (NULL);
+	if (size == 0)
+		return (NULL);
 
-    array = malloc(sizeof(char) * size);
-    if (array == NULL)
-        return (NULL);
+	array = malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 
-    i = 0;
-    while (i < size)
-    {
-        array[i] = c;
-        i++;
-    }
-    return (array);
+	i = 0;
+	while (i < size)
+	{
+		array[i] = c;
+		i++;
+	}
+	return (array);
 }
