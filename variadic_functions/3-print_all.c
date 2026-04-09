0d52f1238d1861ca43f061eb2830865a51e651b3
Jouri-IT <jouri.f.alsulaiman@gmail.com>
2026-04-09 16:12:39 +0000

    Fix: print_all final version

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,31 +1,35 @@
 #include "variadic_functions.h"
 
 /**
- * print_all - prints anything based on a format string
- * @format: types of arguments (c=char, i=int, f=float, s=string)
+ * print_all - prints anything based on format string
+ * @format: list of types of arguments
  * Return: Nothing.
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i;
+	unsigned int j;
 	char *sep;
 	char *s;
+	char *types = "cifs";
 
-	if (!format)
-		return;
 	va_start(args, format);
 	i = 0;
 	sep = "";
-	while (format[i])
+	while (format && format[i])
 	{
-		if (format[i] == 'c')
+		j = 0;
+		s = NULL;
+		while (types[j] && types[j] != format[i])
+			j++;
+		if (j == 0)
 			printf("%s%c", sep, va_arg(args, int)), sep = ", ";
-		if (format[i] == 'i')
+		if (j == 1)
 			printf("%s%d", sep, va_arg(args, int)), sep = ", ";
-		if (format[i] == 'f')
+		if (j == 2)
 			printf("%s%f", sep, va_arg(args, double)), sep = ", ";
-		if (format[i] == 's')
+		if (j == 3)
 		{
 			s = va_arg(args, char *);
 			printf("%s%s", sep, s ? s : "(nil)"), sep = ", ";
