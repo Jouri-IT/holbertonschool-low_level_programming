45bc856da7bcedc17a859ea0e2df072d58efdc24
Jouri-IT <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-04-21 14:12:19 +0300

    Fix task 3: split into helper functions for Betty

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -3,35 +3,38 @@
 #include <stdio.h>
 
 /**
- * main - copies content of a file to another file
- * @ac: argument count
+ * open_files - opens source and destination files
  * @av: argument vector
- *
- * Return: 0 on success, exit with error code on failure
+ * @fd_from: pointer to source file descriptor
+ * @fd_to: pointer to destination file descriptor
  */
-int main(int ac, char **av)
+void open_files(char **av, int *fd_from, int *fd_to)
 {
-	int fd_from, fd_to;
-	char buf[1024];
-	ssize_t r, w;
-
-	if (ac != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
+	*fd_from = open(av[1], O_RDONLY);
+	if (*fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to == -1)
+	*fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (*fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		exit(99);
 	}
+}
+
+/**
+ * copy_content - copies content from one fd to another
+ * @fd_from: source file descriptor
+ * @fd_to: destination file descriptor
+ * @av: argument vector
+ */
+void copy_content(int fd_from, int fd_to, char **av)
+{
+	char buf[1024];
+	ssize_t r, w;
+
 	while ((r = read(fd_from, buf, 1024)) > 0)
 	{
 		w = write(fd_to, buf, r);
@@ -46,6 +49,26 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
+}
+
+/**
+ * main - copies content of a file to another file
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: 0 on success
+ */
+int main(int ac, char **av)
+{
+	int fd_from, fd_to;
+
+	if (ac != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	open_files(av, &fd_from, &fd_to);
+	copy_content(fd_from, fd_to, av);
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
