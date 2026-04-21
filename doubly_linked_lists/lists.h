98ed10409e2f2556da4b279aec7218632b086c5d
Jouri-IT <[jouri.f.alsulaiman@gmail.com](mailto:jouri.f.alsulaiman@gmail.com)>
2026-04-21 13:31:26 +0300

    Fix lists.h and task 1: dlistint_len

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -1,20 +1,24 @@
-#include "lists.h"
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
 
 /**
- * dlistint_len - returns the number of elements in a dlistint_t list
- * @h: pointer to the head of the list
+ * struct dlistint_s - doubly linked list
+ * @n: integer
+ * @prev: points to the previous node
+ * @next: points to the next node
  *
- * Return: the number of nodes
+ * Description: doubly linked list node structure
  */
-size_t dlistint_len(const dlistint_t *h)
+typedef struct dlistint_s
 {
-	size_t count;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
+} dlistint_t;
 
-	count = 0;
-	while (h != NULL)
-	{
-		count++;
-		h = h->next;
-	}
-	return (count);
-}
+size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
+
+#endif /* LISTS_H */
